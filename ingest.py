edd03df9c812f8d91887da3dbe3988defc248e9f
Piyush-Patel19 <hipiyush1108@gmail.com>
2026-06-22 20:43:23 +0530

    Comment out document loading and processing logic
    Comment out the document loading and processing code.

--- ingest.py
+++ ingest.py
@@ -1,27 +1,27 @@
-from langchain_community.document_loaders import TextLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
+# from langchain_community.document_loaders import TextLoader
+# from langchain_text_splitters import RecursiveCharacterTextSplitter
+# from langchain_chroma import Chroma
+# from langchain_huggingface import HuggingFaceEmbeddings
 
-loader = TextLoader("Resume.txt")
-docs = loader.load()
+# loader = TextLoader("Resume.txt")
+# docs = loader.load()
 
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=300,
-    chunk_overlap=50
-)
+# splitter = RecursiveCharacterTextSplitter(
+#     chunk_size=300,
+#     chunk_overlap=50
+# )
 
-chunks = splitter.split_documents(docs)
+# chunks = splitter.split_documents(docs)
 
-embedding = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+# embedding = HuggingFaceEmbeddings(
+#     model_name="sentence-transformers/all-MiniLM-L6-v2"
+# )
 
-db = Chroma.from_documents(
-    documents=chunks,
-    embedding=embedding,
-    collection_name="piyush_portfolio",
-    persist_directory="./chroma_db"
-)
+# db = Chroma.from_documents(
+#     documents=chunks,
+#     embedding=embedding,
+#     collection_name="piyush_portfolio",
+#     persist_directory="./chroma_db"
+# )
 
-print("Data stored successfully")
+# print("Data stored successfully")
